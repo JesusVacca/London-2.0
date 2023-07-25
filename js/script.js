c8b01a79e2703da7d9024c96d711b8e4beacd741
Jesus Vacca <jesus.vacca99@gmail.com>
2023-07-25 12:33:05 -0500

    Subiendo primera parte -V2.0

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const hamburguesa = document.getElementById("hamburguesa");
+const enlaces_catalogo = document.querySelectorAll(".enlaces-catalogo");
+const close_modal = document.getElementById("close-modal");
+
+
+enlaces_catalogo.forEach((cadaEnlace,i)=>{
+    enlaces_catalogo[i].addEventListener("click",(e)=>{
+        e.preventDefault();
+        enlaces_catalogo.forEach((item,j)=>{
+            enlaces_catalogo[j].classList.remove("activo");
+        })
+        enlaces_catalogo[i].classList.add("activo");
+    })
+})
+hamburguesa.onclick = ()=>{
+    document.getElementById("menu-items").classList.toggle("activo");
+}
+close_modal.onclick = ()=>{
+    ocultarModal("none")
+}
+
+function ocultarModal(params) {
+    document.getElementById("ventana-modal").style.display=params;
+}
